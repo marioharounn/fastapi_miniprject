f35051596125268ef2208f76705df192522a727d
mario <mariohar@stud.ntnu.no>
2025-01-14 11:22:19 +0100

    added post func

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Run Docker container: ```docker run -d -p 8000:8000 --name fastapi-container fas
 
 Stop Docker container: ```docker stop fastapi-container```
 Remove containers run:  ```docker rm fastapi-container```
+
+curl -X POST http://127.0.0.1:8000/calculate-sum \
+-H "Content-Type: application/json" \
+-d '{"number1": 10, "number2": 5}'
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class CalculationRequest(BaseModel):
+    number1: float
+    number2: float
+
 @app.get("/")
 def read_root():
-    return{"message:": "Welcome to FastAPI"}
+    return {"message": "Welcome to FastAPI"}
 
 @app.get("/random-endpoint")
 def read_dummy():
     return {"message": "Hello from Append Consulting!"}
+
+@app.post("/calculate-sum")
+def calculate_sum(request: CalculationRequest):
+    result = request.number1 + request.number2
+    return {
+        "Number 1 was": request.number1,
+        "Number 2 was": request.number2,
+        "And the sum becomes": result
+    }
